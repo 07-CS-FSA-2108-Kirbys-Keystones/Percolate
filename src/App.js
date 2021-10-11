521d3e0bfc947bf3a7eb4383f6fb6b4ab5d5dd7c
Matt Stern <mestern@smu.edu>
2021-10-11 12:37:56 -0400

    working crud

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 import React, { useState } from "react";
 import db from "./firebase";
-import { collection, doc, getDocs } from "firebase/firestore"
+import { collection, doc, getDocs, setDoc, updateDoc, deleteDoc } from "firebase/firestore"
 
+//how to CRUD with firestore: https://firebase.google.com/docs/firestore/manage-data/add-data#web-version-9
 async function querySnapshot(){
   let ans = []
+  // collectionRef = collection(db, "businesses"))
+  // documentRef = doc(db, "businesses", "LA")
+  //get
   const businesses = await getDocs(collection(db, "businesses"));
-  businesses.forEach(business=>console.log(business));
+  businesses.forEach(business=>console.log(business.data()));
 
+// Add a new document in collection "businesses" set doc comes with other options and variations like merge option and addDoc variation
+await setDoc(doc(db, "businesses", "LA"), {
+  id: "Los Angeles",
+  title: "CA",
+  description: "USA"
+});
 }
+
+//update a document like so:
+/*await updateDoc(doc(db, "businesses", "LA"), {
+  id: "updatedoc worked"
+});
+
+//delete this document like so:
+await deleteDoc(doc(db, "businesses", "LA"))
+}
+*/
 
 function App() {
   querySnapshot();
